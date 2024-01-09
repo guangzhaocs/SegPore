f7cb162b44154ae56397e4c43ff3116f987a8fe3
guangzhaocs <gz.cheng.cs@gmail.com>
2024-01-09 16:38:04 +0200

    Add files via upload

--- SegPore/scripts/0_data_proc.sh
+++ SegPore/scripts/0_data_proc.sh
@@ -17,7 +17,7 @@ mv demo.gtf 0_reference/
 echo "combine single fast5 files to multi_fast5 ..."
 single_to_multi_fast5 --input_path data/HEK293T-WT-rep1/fast5/ --save_path 1_fast5/ --filename_base HEK293T_WT_rep1_FAK27249_demo --batch_size 4000 --recursive
 
-mkdir 2_fastq/HEK293T_WT_rep1_FAK27249_demo_0
+mkdir 2_fastq/base_calling_res
 mv data 0_origin_data
 
 echo "0_data_proc.sh done ..."
--- SegPore/scripts/1_basecalling.sh
+++ SegPore/scripts/1_basecalling.sh
@@ -2,14 +2,14 @@
 
 cd ..
 
-/scratch/work/chengg1/ont-guppy-cpu/bin/guppy_basecaller -c /scratch/work/chengg1/ont-guppy-cpu/data/rna_r9.4.1_70bps_hac.cfg --num_callers 20 --cpu_threads_per_caller 20 -i demo/1_fast5 -s demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0
+/scratch/work/chengg1/ont-guppy-cpu/bin/guppy_basecaller -c /scratch/work/chengg1/ont-guppy-cpu/data/rna_r9.4.1_70bps_hac.cfg --num_callers 20 --cpu_threads_per_caller 20 -i demo/1_fast5 -s demo/2_fastq/base_calling_res
 echo "base calling done ..."
 
-mkdir demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/all
-cat demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/pass/*.fastq > demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/all/pass.fastq
-cat demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/fail/*.fastq > demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/all/fail.fastq
-cat demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0/all/*.fastq > demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0.fastq
-rm -rf demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0
+mkdir demo/2_fastq/base_calling_res/all
+cat demo/2_fastq/base_calling_res/pass/*.fastq > demo/2_fastq/base_calling_res/all/pass.fastq
+cat demo/2_fastq/base_calling_res/fail/*.fastq > demo/2_fastq/base_calling_res/all/fail.fastq
+cat demo/2_fastq/base_calling_res/all/*.fastq > demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0.fastq
+rm -rf demo/2_fastq/base_calling_res
 
 
 
