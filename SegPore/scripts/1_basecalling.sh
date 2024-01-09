f98c489d4cd68c038c2d827347e3a9d33640dd4f
guangzhaocs <gz.cheng.cs@gmail.com>
2024-01-09 16:39:13 +0200

    Update 1_basecalling.sh

--- SegPore/scripts/1_basecalling.sh
+++ SegPore/scripts/1_basecalling.sh
@@ -10,6 +10,7 @@ cat demo/2_fastq/base_calling_res/pass/*.fastq > demo/2_fastq/base_calling_res/a
 cat demo/2_fastq/base_calling_res/fail/*.fastq > demo/2_fastq/base_calling_res/all/fail.fastq
 cat demo/2_fastq/base_calling_res/all/*.fastq > demo/2_fastq/HEK293T_WT_rep1_FAK27249_demo_0.fastq
 rm -rf demo/2_fastq/base_calling_res
+echo "post-base-calling processing done ..."
 
 
 
