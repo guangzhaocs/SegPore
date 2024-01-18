c7d93407ef1ca87c0fcbea91f69607db9d12975a
guangzhaocs <gz.cheng.cs@gmail.com>
2024-01-18 12:51:44 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@
 # -- Project information
 
 project = 'SegPore'
-copyright = '2024, Guangzhao Cheng'
+copyright = '2024, Guangzhao Cheng, Aki Vehtari, and Lu Cheng'
 author = 'Guangzhao Cheng'
 
 release = '0.1'
