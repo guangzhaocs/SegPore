e186cb5764d9049a1ab2888bd239f3074d9092a4
guangzhaocs <gz.cheng.cs@gmail.com>
2024-01-18 12:45:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SegPore'
+copyright = '2021, Guangzhao Cheng'
+author = 'Guangzhao Cheng'
 
 release = '0.1'
 version = '0.1.0'
