5bbff2d7fdc41fc14cea355bb120b81c85c4de6d
Guangzhao Cheng <gz.cheng.cs@gmail.com>
2024-03-15 13:26:14 +0200

    Update 3_alignment.sh

--- SegPore/scripts/3_alignment.sh
+++ SegPore/scripts/3_alignment.sh
@@ -7,5 +7,6 @@ module load gcc
 cd code/Resquiggle
 ./resquiggle_2D ../../demo/5_align/HEK293T_WT_rep1_FAK27249_demo_0 segpore_eventalign_2D.txt ../../demo/0_reference/model_kmer_m6A_without_header.csv
 
+cd ../..
 python code/2_1_combine_segpore_eventalign.py
 cat demo/5_align/HEK293T_WT_rep1_FAK27249_demo_0/segpore_eventalign_2D_combined.txt | awk -F "\t" '{if($4=="GGACT" ){print $0}}' > demo/5_align/HEK293T_WT_rep1_FAK27249_demo_0/segpore_eventalign_2D_combined_GGACT.txt
