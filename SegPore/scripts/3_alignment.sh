d25628e3fa9e5b6491f32545e4aac18e136e928f
Guangzhao Cheng <gz.cheng.cs@gmail.com>
2024-03-20 10:45:10 +0200

    Update 3_alignment.sh

--- SegPore/scripts/3_alignment.sh
+++ SegPore/scripts/3_alignment.sh
@@ -3,7 +3,6 @@
 cd ..
 python code/2_0_generate_dataset_from_hmm_final.py
 
-module load gcc
 cd code/Resquiggle
 ./resquiggle_2D ../../demo/5_align/HEK293T_WT_rep1_FAK27249_demo_0 segpore_eventalign_2D.txt ../../demo/0_reference/model_kmer_m6A_without_header.csv
 
