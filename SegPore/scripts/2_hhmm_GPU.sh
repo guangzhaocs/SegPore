bf79895dd3a70a28bf82595d623be7808df64af8
Guangzhao Cheng <gz.cheng.cs@gmail.com>
2024-03-20 12:15:09 +0200

    Update 2_hhmm_GPU.sh, add chmod

--- SegPore/scripts/2_hhmm_GPU.sh
+++ SegPore/scripts/2_hhmm_GPU.sh
@@ -1,4 +1,5 @@
 cd ../code/HierHmmCuda
+chmod 777 hmm_one_read 
 for((idx=0;idx<=3999;idx++));  
 do 
    ./hmm_one_read ../../demo/4_hhmm/hhmm_input/HEK293T_WT_rep1_FAK27249_demo_0 ../../demo/4_hhmm/hhmm_output/HEK293T_WT_rep1_FAK27249_demo_0  $idx
